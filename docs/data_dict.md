9059ce0b66ba88545d53568e2f92a29daed00e2f
Luba <luciano.eq@outlook.com>
2023-02-10 04:11:48 -0300

    :sparkles: adding the first pipeline.

---
+++ docs/data_dict.md
@@ -0,0 +1,29 @@
+# De todos os missings que a gente tem...
+
+## Variáveis para usar
+
+- TP_ANTIVIR
+- TOMO_RES
+- TOMO_OUT
+- RAIOX_RES
+- TP_AMOSTRA
+- OUT_AMOSTR
+- MORB_DESC
+- OUT_MORBI
+- OUTRO_DES
+- OUTRO_SIN
+- HEPATICA
+
+## Variáveis para deixar de standby
+
+- OUT_ANIM
+- OBES_IMC
+- M_AMAMENTA
+- MAE_VAC
+- PAC_COCBO
+- PAC_DSCBO
+- SURTO_SG
+
+Premissas:
+
+- Vamos retirar quem tiver de até > 40% de dados missings
---
+++ src/pipelines.py
@@ -0,0 +1,178 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import yaml
+from catboost import CatBoostClassifier
+from lightgbm import LGBMClassifier
+from sklearn.ensemble import HistGradientBoostingClassifier, RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBClassifier
+
+
+@dataclass
+class Params:
+    columns_to_keep: list
+    columns_to_drop: list
+    target_column: str
+    id_column: str
+
+
+class Pipeline:
+    def __init__(self, train_data: Path, test_data: Path, params: Path):
+        self.train_data_path = train_data
+        self.test_data_path = test_data
+        self.params_path = params
+        self.x_test_kaggle = None
+        self.id_test_kaggle = None
+        self.params: Params = self._get_params()
+        self.train_data, self.target = self._get_data()
+
+    def _get_params(self):
+        # load from yaml file
+        with open(self.params_path) as f:
+            params = yaml.safe_load(f)
+        return params
+
+    def _get_data(self):
+        train_data = pd.read_csv(self.train_data_path, low_memory=False)
+        train_data_sampled = train_data[self.params["columns_to_keep"]]
+        target = train_data[self.params["target_column"]]
+
+        return train_data_sampled, target
+
+    def create_train_test_split(self):
+        # split data from scikit-learn
+        x_train, x_test, y_train, y_test = train_test_split(
+            self.train_data,
+            self.target,
+            test_size=0.2,
+            random_state=42,
+            stratify=self.target,
+        )
+        print(x_train.columns)
+        return x_train, x_test, y_train, y_test
+
+    def impute_missing_values(self, x_train, x_test):
+        columns = x_train.columns
+        x_test_kaggle = pd.read_csv(self.test_data_path, low_memory=False)
+        id_column = x_test_kaggle["ID"]
+        x_test_kaggle = x_test_kaggle[self.params["columns_to_keep"]]
+
+        # convert to category
+        nu_idade_column_train = x_train["NU_IDADE_N"].values
+        nu_idade_column_test = x_test["NU_IDADE_N"].values
+        nu_idade_column_test_kaggle = x_test_kaggle["NU_IDADE_N"].values
+
+        x_train.drop(columns=["NU_IDADE_N"], inplace=True)
+        x_test.drop(columns=["NU_IDADE_N"], inplace=True)
+        x_test_kaggle.drop(columns=["NU_IDADE_N"], inplace=True)
+
+        x_train = x_train.astype("category")
+        x_test = x_test.astype("category")
+        x_test_kaggle = x_test_kaggle.astype("category")
+
+        x_train["NU_IDADE_N"] = nu_idade_column_train
+        x_test["NU_IDADE_N"] = nu_idade_column_test
+        x_test_kaggle["NU_IDADE_N"] = nu_idade_column_test_kaggle
+
+        for column in columns:
+            df = x_train[column].value_counts().reset_index()
+            unique_values = df["index"].values
+            df["proportion"] = df[column] / df[column].sum()
+            x_train[column].fillna(
+                np.random.choice(
+                    list(unique_values), 1, p=list(df["proportion"].values)
+                ).item(),
+                inplace=True,
+            )
+            x_test[column].fillna(
+                np.random.choice(
+                    list(unique_values), 1, p=list(df["proportion"].values)
+                ).item(),
+                inplace=True,
+            )
+            x_test_kaggle[column].fillna(
+                np.random.choice(
+                    list(unique_values), 1, p=list(df["proportion"].values)
+                ).item(),
+                inplace=True,
+            )
+
+        self.id_test_kaggle = id_column
+        self.x_test_kaggle = x_test_kaggle
+
+        return x_train, x_test
+
+    def transform_to_numeric(self, x_train, x_test):
+        # transformer CS_SEXO to numeric
+        x_train["CS_SEXO"] = x_train["CS_SEXO"].map({"M": 0, "F": 1, "I": 2})
+        x_test["CS_SEXO"] = x_test["CS_SEXO"].map({"M": 0, "F": 1, "I": 2})
+        return x_train, x_test
+
+    def train_rf(self, x_train, y_train):
+        rf = RandomForestClassifier(
+            n_estimators=1000, random_state=42, class_weight="balanced", n_jobs=10
+        )
+        rf.fit(x_train, y_train.values.ravel())
+        return rf
+
+    def train_lightgbm(self, x_train, y_train):
+        lgbm = LGBMClassifier(
+            n_estimators=1000, random_state=42, class_weight="balanced", n_jobs=10
+        )
+        lgbm.fit(x_train, y_train.values.ravel())
+        return lgbm
+
+    def train_xgboost(self, x_train, y_train):
+        le = LabelEncoder()
+        y_train = le.fit_transform(y_train)
+        xgb = XGBClassifier(
+            n_estimators=1000,
+            random_state=42,
+            class_weight="balanced",
+            n_jobs=10,
+        )
+        xgb.fit(x_train.astype("float64"), y_train.ravel())
+        return xgb
+
+    def train_logistic_regression(self, x_train, y_train):
+        lr = LogisticRegression(
+            max_iter=1000,
+            random_state=42,
+            class_weight="balanced",
+            n_jobs=10,
+        )
+        lr.fit(x_train, y_train.values.ravel())
+        return lr
+
+    def train_catboost(self, x_train, y_train):
+        cat = CatBoostClassifier(
+            n_estimators=1000,
+            random_state=42,
+            # cat_features=[self.params["columns_to_keep"]]
+            # class_weight="balanced",
+            # n_jobs=10,
+        )
+        cat.fit(x_train.astype("float64"), y_train.values.ravel())
+        return cat
+
+    def evaluate_model(self, rf, x_test, y_test):
+        # evaluating the model
+        y_pred = rf.predict(x_test)
+        classification_report_rf = classification_report(y_test, y_pred)
+        print(classification_report_rf)
+
+    def generate_submission_file(self, rf):
+        # generate submission file
+        test_data = self.x_test_kaggle.copy()
+        test_data["CS_SEXO"] = test_data["CS_SEXO"].map({"M": 0, "F": 1, "I": 2})
+
+        y_pred = rf.predict(test_data)
+
+        submission = pd.DataFrame({"ID": self.id_test_kaggle, "CLASSI_FIN": y_pred})
+        submission.to_csv("data/submission_v2.csv", index=False)
